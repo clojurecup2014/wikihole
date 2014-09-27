01398f51ef6c8406e8e26583d9aa9f240fb25d29
Anne Johnson <annekjohnson23@yahoo.com>
2014-09-27 18:35:25 -0400

    Create a plugin stub that actually results in a working plugin
    Next step is make it do stuff!

--- src/plugin/plugin_writer.clj
+++ src/plugin/plugin_writer.clj
@@ -6,30 +6,41 @@
   (:use [hiccup.core]
         [hiccup.page]))
 
-
-(defn write-manifest
+(defn manifest-str
   []
-  (spit "resources/public/plugin/manifest.json"
-        (json/write-str {"name" "Wikihole",
-                         "version" "0.0",
-                         "manifest_version" 2,
-                         "description" "Chart your journey down the Wikihole!",
-                         "browser_action" {
-                                           ;; "default_icon" "icon.png",
-                                           "popup" "index.html"
-                                           }})))
+  (json/write-str
+   {"manifest_version" 2,
+    "name" "Wikihole",
+    "version" "0.0",
+    "description" "Chart your journey down the Wikihole!",
+    "browser_action" {
+                      ;; "default_icon" "icon.png",
+                      "default_popup" "popup.html"
+                      "default_icon" "icon.png"
+                      },
+    "web_accessible_resources" [
+                                "popup.js"],
 
-(defn write-html
+    "permissions" [
+                   "tabs"
+                   "*://*/*"
+                   "contextMenus"
+                   ]}))
+
+
+(defn html-str
   []
-  (spit "resources/public/plugin/index.html"
-        (html5
-         (html
-          [:body
-           [:h1 "Hello, world! This is our Chrome extension. v. 0.0."]]))))
+  (html5
+   (html
+    [:head
+     [:title "Down the Wikihole!"]
+     (include-js "popup.js")]
+    [:body
+     [:h1 "Hello, world! This is the Wikihole Chrome extension. v. 0.0."]])))
 
 (defn write-plugin
   []
-  (write-manifest)
-  (write-html))
+  (spit "resources/public/plugin/manifest.json" (manifest-str))
+  (spit "resources/public/plugin/popup.html" (html-str)))
 
 (write-plugin)
