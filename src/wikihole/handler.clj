7a8a699a19ccadda1778321199b99e2c228ada1d
Kelsey Gilmore-Innis <kelseyq@gmail.com>
2014-09-27 16:08:11 -0700

    separate api & site routes

--- src/wikihole/handler.clj
+++ src/wikihole/handler.clj
@@ -1,21 +1,7 @@
 (ns wikihole.handler
-  (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]
-            [compojure.route :as route]
-            [wikihole.templates :as templates]
-            [wikihole.graphics :as graphics]))
+  (:require [wikihole.api :refer [rest-api]]
+            [wikihole.site :refer [site]]
+            [compojure.core :refer [routes]]))
 
-;; We need sessions and users!
-;; Something we can use for sessions:
-;; https://github.com/ring-clojure/ring/blob/master/ring-core/src/ring/middleware/session.clj
-(defroutes app-routes
-  (GET "/" [] (templates/index))
-  (GET "/user/:id" [id] (templates/user-index id))
-  (GET "/signup" [] (templates/signup))
-  (POST "/signup" [username] (templates/signup-complete username))
-  (GET "/test-chart" [] (templates/default "Test Chart" (graphics/test)))
-  (route/resources "/")
-  (route/not-found "Not Found"))
-
-(def app
-  (handler/site app-routes))
+; Combine the site and rest-api
+(def app (routes rest-api site))
