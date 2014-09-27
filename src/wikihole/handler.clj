196ff44374d718b526a63e122586dc4b3cd1641e
Anne Johnson <annekjohnson23@yahoo.com>
2014-09-26 23:40:24 -0400

    Create some routes and some template methods; add Foundation stylesheet

--- src/wikihole/handler.clj
+++ src/wikihole/handler.clj
@@ -2,13 +2,18 @@
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
-            [wikihole.templates :refer :all]))
+            [wikihole.templates :as templates]))
 
+;; We need sessions and users!
+;; Something we can use for sessions:
+;; https://github.com/ring-clojure/ring/blob/master/ring-core/src/ring/middleware/session.clj
 (defroutes app-routes
-  (GET "/" [] (wikihole.templates/default))
+  (GET "/" [] (templates/index))
+  (GET "/user/:id" [id] (templates/user-index id))
+  (GET "/signup" [] (templates/signup))
+  (POST "/signup" [username] (templates/signup-complete username))
   (route/resources "/")
   (route/not-found "Not Found"))
-
 
 (def app
   (handler/site app-routes))
--- src/wikihole/templates.clj
+++ src/wikihole/templates.clj
@@ -1,16 +1,45 @@
 (ns wikihole.templates)
 
-(use 'hiccup.core 'hiccup.page)
+(use 'hiccup.core 'hiccup.page 'hiccup.form)
 
 (defn head
-  []
-  (html [:head [:title "Down the Wikihole!"]]))
-
-(defn body
-  []
-  (html [:body [:h1 "Welcome to the Wikihole!"]
-         [:p "Enjoy your ride. We'll chart your adventures."]]))
+  "Builds <head>"
+  [title]
+  [:head [:title title]
+         (include-css "/normalize.css" "/foundation.min.css")])
 
 (defn default
+  "Default template. Wraps content argument in HTML5 doc."
+  [content]
+  (html
+   (html5
+    (head "Down the Wikihole!")
+    [:body content])))
+
+(defn signup-form
+  "Builds the HTML for a signup form"
   []
-  (html (html5 (head) (body))))
+  (html (form-to [:post "/signup"]
+               (label "username" "Username")
+               (text-field "username")
+               (label "password" "Password")
+               (password-field "password")
+               (submit-button {:class "button primary"} "Signup"))))
+
+(defn index
+  []
+  (default (html [:h1 "Welcome to the Wikihole!"]
+                 [:p (str "Enjoy your ride."
+                          " We'll chart your adventures.")])))
+
+(defn signup
+  []
+  (default (signup-form)))
+
+(defn signup-complete
+  [username]
+  (default (str "Thanks for signing up, " username "!")))
+
+(defn user-index
+  [id]
+  (default (str "Hello, User " id "!")))
