00e2145b3df506fbf10530e7ec195bf2f46e6311
Kelsey Gilmore-Innis <kelseyq@gmail.com>
2014-09-27 17:52:43 -0700

    dummy trip api

--- src/wikihole/api.clj
+++ src/wikihole/api.clj
@@ -1,10 +1,17 @@
 (ns wikihole.api
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.middleware.json :as middleware]
+            [wikihole.data-access :as data-access]))
 
 (defroutes api-routes
-  (GET "/trip/:id" [id] (str "trip for " id)))
+  (POST "/user/:userId/trip" {params :params body :body} (data-access/save-trip (:userId params) body))
+  (GET "/trip/:tripId" [tripId] (data-access/get-trip tripId))
+  (GET "/user/:userId/trips" [userId] (data-access/get-trips-for-user userId)))
+
 
 (def rest-api
-  (handler/api api-routes))
+  (-> (handler/api api-routes)
+        (middleware/wrap-json-body)
+        (middleware/wrap-json-response)))
